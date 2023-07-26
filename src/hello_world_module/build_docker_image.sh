ec8d057521eb0e280809601de61c4d0754685ff6
Yongzhou Zhang <zhou2300@gmail.com>
2023-07-26 23:25:24 +0200

    update docher hub repo address

--- src/hello_world_module/build_docker_image.sh
+++ src/hello_world_module/build_docker_image.sh
@@ -1,2 +1,2 @@
 cd .. 
-docker build -f hello_world_module/Dockerfile -t metagoto/ros2-basic-examples/hello_world:v1-param .
+docker build -f hello_world_module/Dockerfile -t ros2-basic-examples:v1-param .
